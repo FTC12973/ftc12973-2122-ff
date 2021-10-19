1883b1b787c383f37a7a295ded543d75db1249ba
FTC12973 <ftc12973@bishopreicher.com>
2021-10-19 17:28:42 -0500

    Initial Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLinearOpMode.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
+//@Disabled
 public class BasicLinearOpMode extends LinearOpMode {
 
     // Declare OpMode members.
